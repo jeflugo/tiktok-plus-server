6ef3b879a40b545792eee207341804f684323547
jeflugo <jefersoneduardoluego@gmail.com>
2024-07-24 20:57:27 -0400

    update controller

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,45 +1,30 @@
 import { Request, Response } from 'express'
 import service from '../services/users'
+import { tryCatchWrapper } from '../utils/tryCatchWrapper'
 
-const getUsers = async (req: Request, res: Response) => {
-	try {
-		const users = await service.getUsers()
-		res.json(users)
-	} catch (e) {
-		res.status(500).json('GET_USERS_ERROR')
-	}
-}
-const getUser = async (req: Request, res: Response) => {
-	try {
-		const user = await service.getUser(req.params.id)
-		res.json(user)
-	} catch (e) {
-		res.status(500).json('GET_USER_ERROR')
-	}
-}
-const postUser = async (req: Request, res: Response) => {
-	try {
-		const newUser = await service.postUser(req.body)
-		res.json(newUser)
-	} catch (e) {
-		res.status(500).json('POST_USER_ERROR')
-	}
-}
-const upadateUser = async (req: Request, res: Response) => {
-	try {
-		const user = await service.updateUser(req.params.id, req.body)
-		res.json(user)
-	} catch (e) {
-		res.status(500).json('UPDATE_USER_ERROR')
-	}
-}
-const deleteUser = async (req: Request, res: Response) => {
-	try {
-		const users = await service.deleteUser(req.params.id)
-		res.json(users)
-	} catch (e) {
-		res.status(500).json('DELETE_USER_ERROR')
-	}
-}
+const getUsers = tryCatchWrapper(async (req: Request, res: Response) => {
+	const users = await service.getUsers()
+	res.json(users)
+}, 'GET_USERS_SERVER_ERROR')
+
+const getUser = tryCatchWrapper(async (req: Request, res: Response) => {
+	const user = await service.getUser(req.params.id)
+	res.json(user)
+}, 'GET_USER_SERVER_ERROR')
+
+const postUser = tryCatchWrapper(async (req: Request, res: Response) => {
+	const newUser = await service.postUser(req.body)
+	res.json(newUser)
+}, 'POST_USER_SERVER_ERROR')
+
+const upadateUser = tryCatchWrapper(async (req: Request, res: Response) => {
+	const user = await service.updateUser(req.params.id, req.body)
+	res.json(user)
+}, 'UPDATE_USER_SERVER_ERROR')
+
+const deleteUser = tryCatchWrapper(async (req: Request, res: Response) => {
+	const users = await service.deleteUser(req.params.id)
+	res.json(users)
+}, 'DELETE_USER_SERVER_ERROR')
 
 export { getUsers, getUser, postUser, upadateUser, deleteUser }
